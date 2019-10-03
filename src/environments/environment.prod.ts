35ed67fb87e71eb5bfd204956540fa6fbfb8d454
Ronnie <53875082+theronniedsouza@users.noreply.github.com>
2019-10-03 12:31:56 +0530

    env variable update for prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
   apiUrl: 'http://dsdvans.co.uk/api/',
-  snackBarTimeOut: 3000
+  snackBarTimeOut: 3000,
+  placeHolderText: 'Data will appear here soon'
 };
